7ab19773418686245b405239b80b1b5ab9e2c408
Thiago Oliveira <thiago.oliveira@mult-e.com.br>
2018-07-07 20:55:14 -0300

    Cria a tela principal

---
+++ app/src/main/java/br/com/thideoli/vendedormovel/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.thideoli.vendedormovel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void listOrders(View view) {
+    }
+
+    public void listCustomers(View view) {
+    }
+
+    public void listProducts(View view) {
+    }
+}
