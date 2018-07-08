66b8d7d979ff2d2bfe379d7c208da5414fdd5459
Thiago Oliveira <thiago.oliveira@mult-e.com.br>
2018-07-08 11:50:04 -0300

    Adiciona funcionalidade para deslogar

--- app/src/main/java/br/com/thideoli/vendedormovel/MainActivity.java
+++ app/src/main/java/br/com/thideoli/vendedormovel/MainActivity.java
@@ -1,7 +1,11 @@
 package br.com.thideoli.vendedormovel;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +30,36 @@ public class MainActivity extends AppCompatActivity {
 
         emailLabel = findViewById(R.id.emailLabel);
         emailLabel.setText(user.getEmail());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.main_logout:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logout() {
+        firebaseAuth.signOut();
+        chamaTelaLogin();
+    }
+
+    private void chamaTelaLogin() {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void listOrders(View view) {
